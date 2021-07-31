0a186511289a362f119f15437433a18d1112faab
TBM85 <tballesterm@gmail.com>
2021-07-31 10:29:48 -0400

    Include a logout button on the content page

--- src/components/ContentPage/ContentPage.js
+++ src/components/ContentPage/ContentPage.js
@@ -1,3 +1,4 @@
+import Button from "components/UI/Button/Button";
 import React from "react";
 
 import classes from "./ContentPage.module.scss";
@@ -6,6 +7,7 @@ const ContentPage = () => {
   return (
     <div className={classes.ContentPage}>
       <h2>Account created, you are now a registered user</h2>
+      <Button type="button" className={classes.LogoutBtn}>Logout</Button>
     </div>
   );
 };
