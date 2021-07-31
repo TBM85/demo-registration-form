f96d181ad5bee051c360ff7383995c49e8f28e6b
TBM85 <tballesterm@gmail.com>
2021-07-31 09:31:32 -0400

    Create a component for content page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.scss";
 import Form from "components/Form/Form";
+import ContentPage from "components/ContentPage/ContentPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <h1>Registration Form</h1>
       </header>
       <Form />
+      <ContentPage />
     </div>
   );
 }
---
+++ src/components/ContentPage/ContentPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "./ContentPage.module.scss";
+
+const ContentPage = () => {
+  return (
+    <div className={classes.ContentPage}>
+      <h2>Account created, you are now a registered user</h2>
+    </div>
+  );
+};
+
+export default ContentPage;
