581f481752058a2c831841e085a08abeb66fd02e
TBM85 <tballesterm@gmail.com>
2021-08-09 09:53:29 +0200

    Translate the content page

--- src/components/ContentPage/ContentPage.js
+++ src/components/ContentPage/ContentPage.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
 import classes from "./ContentPage.module.scss";
 import Button from "components/UI/Button/Button";
 
 const ContentPage = (props) => {
   const { submitted } = props;
+
+  const [t] = useTranslation("global");
+
   const [isSubmitted, setIsSubmitted] = useState(submitted);
 
   // Close the content page and the registration form appears
@@ -21,13 +25,13 @@ const ContentPage = (props) => {
   return (
     <div className={classes.ContentPage}>
       <div className={classes["like"]}></div>
-      <h2>Account created, you are now a registered user</h2>
+      <h2>{t("content-page.text")}</h2>
       <Button
         type="button"
         className={classes.LogoutBtn}
         onClick={logoutHandler}
       >
-        Logout
+        {t("content-page.logout-button")}
       </Button>
     </div>
   );
