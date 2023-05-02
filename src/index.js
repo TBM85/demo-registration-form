05ba8e6f0a41cf4b1c565575a7b105d34719ce63
Tania Ballester <tballester.email@gmail.com>
2023-05-02 22:53:53 +0200

    refactor: using createRoot instead of ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -22,13 +22,15 @@ i18next.init({
   }
 });
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
       <App />
     </I18nextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
